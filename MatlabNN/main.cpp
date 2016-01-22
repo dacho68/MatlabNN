834b345c6d03eac5513c796751d78e6d7f5b5517
Terna Kpamber <tkpamber@yahoo.com>
2016-01-22 12:18:39 -0800

    Initial Commit

---
+++ MatlabNN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MatlabNN
+//
+//  Created by Terna Kpamber on 1/22/16.
+//  Copyright © 2016 Terna Kpamber. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
